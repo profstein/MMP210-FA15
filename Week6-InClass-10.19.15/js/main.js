9168c95c4f792cad6aadda8274856f8b43adc528
Christopher Stein <cstein@bmcc.cuny.edu>
2015-10-21 03:28:12 -0400

    Complex drawWindow function

--- Week6-InClass-10.19.15/js/main.js
+++ Week6-InClass-10.19.15/js/main.js
@@ -1,18 +1,64 @@
+//global variables
+var myWinWidth, myWinHeight;
+
 function setup(){
     //create the canvas. put into a variable to associate with a element
-    var cnv = createCanvas(800,600);
+    var cnv = createCanvas(800,3000);
     cnv.parent("windows");
     
-    //draw a window by calling the drawWindow function
-    drawWindow(50,50);
+    //set the width and height of the window i will draw
+    myWinHeight = 250;
+    myWinWidth = 100;
     
-    drawWindow(200,50);
+    var colWidth = 160;
+    var rowHeight = 290;
+    var x = 0;
+    var y = 0;
     
-    
-    drawWindow(350,50);
-}
+    for(var i=0; i<100; i++){
+        drawWindow(x,y);
+        x += colWidth;
+        
+        if(x > width-myWinWidth){
+            x = 0;
+            y += rowHeight;
+        }
+    }//end for loop
+}//end setup
 
 function drawWindow(startX, startY){
     fill('peru');
-    rect(startX, startY,100,250);
+    rect(startX, startY,myWinWidth,myWinHeight);
+    
+    stroke('black');
+   var x1,x2,y1,y2;
+    x1 = startX+myWinWidth/2; //middle of window
+    y1 = startY;
+    x2 = startX+myWinWidth/2;
+    y2 = startY + myWinHeight;
+    line(x1,y1,x2,y2 );
+    
+    //horizontal line across the top
+    x1 = startX; //middle of window
+    y1 = startY+myWinHeight*.333;
+    x2 = startX+myWinWidth;
+    y2 = y1;
+    line(x1,y1,x2,y2 );
+    
+    //horizontal line 2/3 of the way down
+    x1 = startX; //middle of window
+    y1 = startY+myWinHeight*.666;
+    x2 = startX+myWinWidth;
+    y2 = y1;
+    line(x1,y1,x2,y2 );
+    
+    //add box at the bottom
+    x1 = startX - 20;
+    y1 = startY + myWinHeight;
+    rect(x1,y1,myWinWidth+40, 20);
+    
+    
+    
+    
+    
 }
