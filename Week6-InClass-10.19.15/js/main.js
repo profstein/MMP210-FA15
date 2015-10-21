a2f243797dd95c20f9fb4ce0c7e31ab33d5792a5
Christopher Stein <cstein@bmcc.cuny.edu>
2015-10-21 02:43:50 -0400

    Week 6 Initial Commit. Files and basic window drawn

---
+++ Week6-InClass-10.19.15/js/main.js
@@ -0,0 +1,11 @@
+
+function setup(){
+    //create the canvas. Here we are putting it into a variable so we can associate it with an element.
+    //When you are doing things like putting items in a grid, you may want the canvas dimensions to be specific numbers and not just windowWidth and windowHeight
+    var cnv = createCanvas(800,600);
+    cnv.parent("windows");
+    
+    //draw a window
+    fill('peru');
+    rect(50,50,100,250);
+}
