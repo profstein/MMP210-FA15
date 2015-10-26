0afadcfdae580562add45226d43d53bb2676f310
Christopher Stein <cstein@f1006-i-34171.bmcc.cuny.edu>
2015-10-26 15:04:30 -0400

    For loops to create and loop through an array

--- Week7-InClass-10.26.15/js/main.js
+++ Week7-InClass-10.26.15/js/main.js
@@ -5,12 +5,25 @@ function setup(){
     var cnv = createCanvas(800,3000);
     cnv.parent("windows");
 
-    var myArray = ["hello",3,"bye"];
-    console.log(myArray[0]);
-    console.log(myArray[1]);
-    console.log(myArray[2]);
+    var myArray = []; //create blank array
+    //add stuff to the array with a loop
+    for(var i=0; i<10000; i++){
+//        myArray[i] = "item " + i;
+        myArray[i] = i;
+    }
+
+    //log out the array
     console.log(myArray);
-    console.log("myArray", myArray);
+
+    //Loop to go through every item in the array
+    for(var i=0; i < myArray.length; i++){
+        myArray[i]+=10;
+//        console.log(myArray[i]);
+    }
+
+    console.log(myArray);
+
+
 
 }//end setup
 
