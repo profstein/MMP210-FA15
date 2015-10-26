0f74b791992ed901f738e27c267adce457ef78d6
Christopher Stein <cstein@f1006-i-34171.bmcc.cuny.edu>
2015-10-26 14:44:43 -0400

    Entering basic array creation and logging

--- Week7-InClass-10.26.15/js/main.js
+++ Week7-InClass-10.26.15/js/main.js
@@ -5,5 +5,12 @@ function setup(){
     var cnv = createCanvas(800,3000);
     cnv.parent("windows");
 
+    var myArray = ["hello",3,"bye"];
+    console.log(myArray[0]);
+    console.log(myArray[1]);
+    console.log(myArray[2]);
+    console.log(myArray);
+    console.log("myArray", myArray);
+
 }//end setup
 
