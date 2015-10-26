2ca23fc9ed305dd35bd8b03c3b3923661038ae71
Christopher Stein <cstein@f1006-i-34171.bmcc.cuny.edu>
2015-10-26 16:59:29 -0400

    adding in the start of checking to see if the mouse is in the circle

--- Week7-InClass-10.26.15/js/main.js
+++ Week7-InClass-10.26.15/js/main.js
@@ -1,5 +1,5 @@
 //global variables
-
+var myArray = []; //create blank array
 
 //Bubble Constructor
 function Bubble(){
@@ -15,35 +15,29 @@ function Bubble(){
     }
 }//end Bubble
 
-
 function setup(){
     //create the canvas. put into a variable to associate with a element
     var cnv = createCanvas(windowWidth,windowHeight);
     cnv.parent("windows");
 
-    var myArray = []; //create blank array
     //add stuff to the array with a loop
-    for(var i=0; i<10; i++){
+    for(var i=0; i<100; i++){
         //add Bubbles to the array
         myArray[i] = new Bubble();
         myArray[i].drawBubble();
     }
-
-    //log out the array
-    console.log(myArray);
-
-    //Loop to go through every item in the array
-    for(var i=0; i < myArray.length; i++){
-//        myArray[i]+=10;
-        console.log(myArray[i]);
-    }
-
-    console.log(myArray);
-
 }//end setup
 
 //listen for mouse presses with the built-in p5 mousePressed method
 function mousePressed(){
     //check that this is working
     console.log("mouse Pressed");
-}
+
+    //loop through myArray and check if mouse is in each Bubble
+    var d;
+    for(var i=0; i < myArray.length; i++){
+        //myArray[i].x myArray[i].r
+        d = dist(mouseX, mouseY, myArray[i].x, myArray[i].y);
+        console.log("distance",d);
+    }
+}//end mousePressed
