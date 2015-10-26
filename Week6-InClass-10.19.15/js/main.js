c826213f741c9febd9bdc0c7aca6d0b02fa232f5
Christopher Stein <cstein@f1006-i-34171.bmcc.cuny.edu>
2015-10-26 14:01:37 -0400

    adding in window lines

--- Week6-InClass-10.19.15/js/main.js
+++ Week6-InClass-10.19.15/js/main.js
@@ -2,18 +2,59 @@ function setup() {
 
     createCanvas(windowWidth, windowHeight);
 
-    //  drawWindow(50,50); //call the function
-    //    drawWindow(200,50); //call the function
-    //    drawWindow(350,50); //call the function
-
+    var x = 0;
+    var y = 0;
+    var gridWidth = width - 100;
     for (var i = 0; i < 100; i++) {
         console.log(i);
-        drawWindow(50 * i, 50);
-    }
+        drawWindow(x, y);
+        //increment our X
+        x += 120; //same as x = x + 120;
+        //check if x is past the grid width
+        if (x > gridWidth) {
+            //set x back to the beginning
+            x = 0;
+            //move y down
+            y += 270;
+        }//end if
+    }//end for loop
+
+}//end setup
+
+function draw() {
+
 
 }
 
 function drawWindow(startX, startY) {
     fill('peru');
-    rect(startX, startY, 100, 250);
+    var winWidth = 100;
+    var winHeight = 250;
+    rect(startX, startY, winWidth, winHeight);
+
+    //window lines
+    line(startX + winWidth/2, startY, startX + winWidth/2, startY + winHeight);
+
+    line(startX, startY *.333, startX + winWidth, startY * .333);
+
+    line(startX, startY *.666, startX + winWidth, startY * .666);
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
